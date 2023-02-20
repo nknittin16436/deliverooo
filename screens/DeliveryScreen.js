2e63e1ec9ec1b7da5ba61c3a8876c80d81a2ccd7
Nand <nknittin16436@gmail.com>
2023-02-20 19:04:07 +0530

    The final delivery screen with maps done

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,12 +1,94 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectRestaurant } from '../features/restaurantSlice';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Image } from 'react-native';
+import * as Progress from 'react-native-progress';
+import MapView, { Marker } from 'react-native-maps';
 
 const DeliveryScreen = () => {
-  return (
-    <View>
-      <Text>DeliveryScreen</Text>
-    </View>
-  )
+
+    const navigation = useNavigation();
+    const restaurant = useSelector(selectRestaurant)
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    })
+    return (
+        <View className="flex-1 bg-[#00CCBB]">
+            <SafeAreaView className="z-50">
+                <View className="flex-row justify-between p-5 items-center" >
+                    <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                        <MaterialIcons name="cancel" size={30} color="white" />
+                    </TouchableOpacity>
+
+                    <Text className="text-lg font-light text-white">Order Help</Text>
+                </View>
+                <View className="bg-white mx-5 my-2 rounded-md p-6 z-50 shadow-md">
+                    <View className="flex-row justify-between">
+
+                        <View cla>
+                            <Text className="text-lg text-gray-400">Esimated Arrival</Text>
+                            <Text className="text-3xl font-bold">40-55 Minutes</Text>
+                        </View>
+                        <Image
+                            source={{ uri: "https://links.papareact.com/fls" }}
+                            className="h-20 w-20"
+                        />
+                    </View>
+                    <Progress.Bar width={150} indeterminate={true} color="#00CCBB" />
+                    <Text className="mt-3 text-gray-500">Your order at {restaurant.title} is being Prepared</Text>
+                </View>
+            </SafeAreaView>
+            <MapView
+                initialRegion={{
+                    longitude: restaurant.long,
+                    latitude: restaurant.lat,
+                    longitudeDelta: 0.005,
+                    latitudeDelta: 0.005
+
+                }}
+                className="flex-1 mt-10 z-0"
+                mapType='mutedStandard'
+            >
+
+                <Marker
+                    coordinate={{
+                        longitude: restaurant.long,
+                        latitude: restaurant.lat
+
+                    }}
+                    title={restaurant.title}
+                    description={restaurant.short_description}
+                    identifier='origin'
+                    pinColor='#00CCBB'
+                />
+            </MapView>
+
+            <SafeAreaView className="bg-white flex-row items-center space-x-5 h-28">
+                <Image
+                    source={{
+                        uri: "https://links.papareact.com/wru"
+                    }}
+                    className="h-12 w-12 p-4 rounded-full ml-5 bg-gray-300"
+                />
+                <View className="flex-1">
+                    <Text className="text-lg">
+                        Nand Kumar
+                    </Text>
+                    <Text className="text-gray-400">
+                        Your Rider
+                    </Text>
+                </View>
+                <Text className="text-[#00CCBB] text-lg mr-5 font-bold">Call</Text>
+            </SafeAreaView>
+        </View>
+    )
 }
 
 export default DeliveryScreen
--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -31,12 +31,12 @@ const RestaurantScreen = () => {
     }, []);
 
     useEffect(() => {
-    //     sanityClient.fetch(`
-    //         *[_type == "restaurant" && _id==$id]{
-    //             ...,
-    //             dishes[]->
-    //           }
-    //   `, { id }).then(data => { setDishes(data[0].dishes) }).catch(error => console.log(error))
+        //     sanityClient.fetch(`
+        //         *[_type == "restaurant" && _id==$id]{
+        //             ...,
+        //             dishes[]->
+        //           }
+        //   `, { id }).then(data => { setDishes(data[0].dishes) }).catch(error => console.log(error))
 
 
         dispatch(setRestaurant({
@@ -47,67 +47,69 @@ const RestaurantScreen = () => {
             genre,
             address,
             short_description,
-            dishes
+            dishes,
+            long,
+            lat
         }))
     }, [dispatch]);
     return (
         // <SafeAreaView>
-            <>
-                <BasketIcon />
-                <ScrollView>
-                    <View className="relative">
-                        <Image
-                            source={
-                                {
-                                    uri: urlFor(imgUrl).url()
-                                }
+        <>
+            <BasketIcon />
+            <ScrollView>
+                <View className="relative">
+                    <Image
+                        source={
+                            {
+                                uri: urlFor(imgUrl).url()
                             }
-                            className="w-full h-56 bg-gray-34 p-4"
-                        />
-                        <TouchableOpacity onPress={() => navigation.goBack()} className="absolute top-6 left-5 p-2 bg-gray-100 rounded-full">
-                            <AntDesign name="arrowleft" size={24} color="#00CCBB" />
-                        </TouchableOpacity>
-                    </View>
-
-                    <View className="bg-white">
-                        <View className="px-4 pt-4">
-                            <Text className="text-3xl font-bold">{title}</Text>
-                            <View className="flex-row space-x-2 my-1">
-                                <View className="flex-row items-center space-x-1">
-                                    <AntDesign name="star" size={16} color="green" />
-                                    <Text className="text-xs text-gray-500">
-
-                                        <Text className="text-green-500">{rating}</Text> .{genre}{' '}
-                                    </Text>
-                                </View>
-                                <View className="flex-row items-center space-x-1">
-                                    <Feather name="map-pin" size={20} color="gray" />
-                                    <Text className="text-xs text-gray-500">
-                                        {address}
-                                    </Text>
-                                </View>
-                            </View>
-                            <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
-                        </View>
-                        <TouchableOpacity className="flex-row p-4 items-center space-x-2 border-y border-gray-300">
-                            <AntDesign name="question" size={20} color="gray" />
-                            <Text className="pl-2 flex-1 text-md font-bold"> Have a food alergy..?</Text>
-                            <AntDesign name="right" size={24} color="#00CCBB" />
-                        </TouchableOpacity>
-                    </View>
-
-                    <View className="pb-36">
-                        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
-
-
-                        {
-                            dishes?.map((dish) => (
-                                <DishRow key={dish._id} dish={dish} />
-                            ))
                         }
+                        className="w-full h-56 bg-gray-34 p-4"
+                    />
+                    <TouchableOpacity onPress={() => navigation.goBack()} className="absolute top-6 left-5 p-2 bg-gray-100 rounded-full">
+                        <AntDesign name="arrowleft" size={24} color="#00CCBB" />
+                    </TouchableOpacity>
+                </View>
+
+                <View className="bg-white">
+                    <View className="px-4 pt-4">
+                        <Text className="text-3xl font-bold">{title}</Text>
+                        <View className="flex-row space-x-2 my-1">
+                            <View className="flex-row items-center space-x-1">
+                                <AntDesign name="star" size={16} color="green" />
+                                <Text className="text-xs text-gray-500">
+
+                                    <Text className="text-green-500">{rating}</Text> .{genre}{' '}
+                                </Text>
+                            </View>
+                            <View className="flex-row items-center space-x-1">
+                                <Feather name="map-pin" size={20} color="gray" />
+                                <Text className="text-xs text-gray-500">
+                                    {address}
+                                </Text>
+                            </View>
+                        </View>
+                        <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
                     </View>
-                </ScrollView>
-            </>
+                    <TouchableOpacity className="flex-row p-4 items-center space-x-2 border-y border-gray-300">
+                        <AntDesign name="question" size={20} color="gray" />
+                        <Text className="pl-2 flex-1 text-md font-bold"> Have a food alergy..?</Text>
+                        <AntDesign name="right" size={24} color="#00CCBB" />
+                    </TouchableOpacity>
+                </View>
+
+                <View className="pb-36">
+                    <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+
+
+                    {
+                        dishes?.map((dish) => (
+                            <DishRow key={dish._id} dish={dish} />
+                        ))
+                    }
+                </View>
+            </ScrollView>
+        </>
         // </SafeAreaView>
     )
 }
